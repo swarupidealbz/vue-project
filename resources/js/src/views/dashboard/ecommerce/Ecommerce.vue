c5bdd98796af7e147d24f2b7eba494fec07c6e68
swarup <swarup@idealabz.biz>
2022-02-03 01:54:12 +0530

    fix js issue

--- resources/js/src/views/dashboard/ecommerce/Ecommerce.vue
+++ resources/js/src/views/dashboard/ecommerce/Ecommerce.vue
@@ -141,6 +141,7 @@ export default {
         let userData = JSON.parse(localStorage.getItem('userData'));
         stat = userData.stat.statistics;
       }
+      console.log(stat);
       return stat;
     }
   },
