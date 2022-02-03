9ca3416f0f4ea8cb8bc2e0ac593bd03b76abc8ce
swarup <swarup@idealabz.biz>
2022-02-03 18:05:22 +0000

    url update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -227,7 +227,7 @@ class AuthController extends BaseController
 					'status' => false,
 					'message' => "The link seems to have expired, please check or reset your password again."
 				];
-				return redirect()->to(env('APP_FRONTEND_URL'))->withCookie(cookie('password_reset', json_encode($response), time() + 10*60, '/', '99ideaz.com', false, false));
+				return redirect()->to(env('APP_URL'))->withCookie(cookie('password_reset', json_encode($response), time() + 10*60, '/', '99ideaz.com', false, false));
             }
 
             $user = User::select('id','first_name', 'last_name', 'mobile', 'email', 'role')
@@ -245,7 +245,7 @@ class AuthController extends BaseController
 					'status' => true,
 					'message' => "You're password has been reset successfully and sent on email, please check."
 				];
-				return redirect()->to(env('APP_FRONTEND_URL'))->withCookie(cookie('password_reset', json_encode($response), time() + 10*60, '/', '99ideaz.com', false, false));
+				return redirect()->to(env('APP_URL'))->withCookie(cookie('password_reset', json_encode($response), time() + 10*60, '/', '99ideaz.com', false, false));
             }          
                 
             //return redirect()->to(env('APP_FRONTEND_URL'))->withCookie(cookie('password_reset', false));
@@ -253,7 +253,7 @@ class AuthController extends BaseController
 				'status' => false,
 				'message' => "You're password reset failed, please try again later."
 			];
-			return redirect()->to(env('APP_FRONTEND_URL'))->withCookie(cookie('password_reset', json_encode($response), time() + 10*60, '/', '99ideaz.com', false, false));
+			return redirect()->to(env('APP_URL'))->withCookie(cookie('password_reset', json_encode($response), time() + 10*60, '/', '99ideaz.com', false, false));
             
         }
         catch(Exception $e) {
