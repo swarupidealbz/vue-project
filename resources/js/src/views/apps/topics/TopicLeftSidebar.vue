a26ffe4c71f2efb41b4fa3554b8694f0488132b7
swarup <swarup@idealabz.biz>
2022-02-04 23:59:36 +0530

    set website

--- resources/js/src/views/apps/topics/TopicLeftSidebar.vue
+++ resources/js/src/views/apps/topics/TopicLeftSidebar.vue
@@ -13,10 +13,10 @@
               Compose
             </b-button>
           </div>
-          <vue-perfect-scrollbar
+          <!-- <vue-perfect-scrollbar
             :settings="perfectScrollbarSettings"
             class="sidebar-menu-list scroll-area"
-          >
+          > -->
             <!-- Filters -->
             <b-list-group class="list-group-messages">
               <b-list-group-item
@@ -28,52 +28,9 @@
               >
                 <span class="align-text-bottom line-height-1">{{ group.name }}</span>                
               </b-list-group-item>
-              <!-- <b-list-group-item
-                v-for="filter in emailFilters"
-                :key="filter.title + $route.path"
-                :to="filter.route"
-                :active="isDynamicRouteActive(filter.route)"
-                @click="$emit('close-left-sidebar')"
-              >
-                <feather-icon
-                  :icon="filter.icon"
-                  size="18"
-                  class="mr-75"
-                />
-                <span class="align-text-bottom line-height-1">{{ filter.title }}</span>
-                <b-badge
-                  v-if="emailsMeta[filter.title.toLowerCase()]"
-                  pill
-                  :variant="resolveFilterBadgeColor(filter.title)"
-                  class="float-right"
-                >
-                  {{ emailsMeta[filter.title.toLowerCase()] }}
-                </b-badge>
-              </b-list-group-item> -->
             </b-list-group>
 
-            <!-- Labels -->
-            <h6 class="section-label mt-3 mb-1 px-2">
-              Labels
-            </h6>
-
-            <b-list-group class="list-group-labels">
-              <b-list-group-item
-                v-for="label in emailLabel"
-                :key="label.title + $route.path"
-                :to="label.route"
-                :active="isDynamicRouteActive(label.route)"
-                @click="$emit('close-left-sidebar')"
-              >
-                <span
-                  class="bullet bullet-sm mr-1"
-                  :class="`bullet-${label.color}`"
-                />
-                <span>{{ label.title }}</span>
-              </b-list-group-item>
-            </b-list-group>
-
-          </vue-perfect-scrollbar>
+          <!-- </vue-perfect-scrollbar> -->
         </div>
       </div>
     </div>
