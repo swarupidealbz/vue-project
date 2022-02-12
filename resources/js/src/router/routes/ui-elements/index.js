a5f320e79cde9092c3036a2f77a55470b5b79fbd
swarup <swarup@idealabz.biz>
2022-02-12 17:48:55 +0530

    comment add

--- resources/js/src/router/routes/ui-elements/index.js
+++ resources/js/src/router/routes/ui-elements/index.js
@@ -1,9 +1,8 @@
-import cards from './cards'
 import components from './components'
 import extensions from './extensions'
 import ui from './ui'
 import pageLayouts from './page-layouts'
 
-const data = [...cards, ...components, ...extensions, ...ui, ...pageLayouts]
+const data = [...components, ...extensions, ...ui, ...pageLayouts]
 
 export default data
