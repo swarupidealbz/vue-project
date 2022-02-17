2a5aac99d2c477fa274e944412403038568e5e05
swarup <swarup@idealabz.biz>
2022-02-17 23:52:59 +0530

    notification checkbox

--- resources/js/src/views/pages/profile/AccountSetting.vue
+++ resources/js/src/views/pages/profile/AccountSetting.vue
@@ -44,62 +44,7 @@
     </b-tab>
     <!--/ change password tab -->
 
-    <!-- info -->
-    <b-tab>
-
-      <!-- title -->
-      <template #title>
-        <feather-icon
-          icon="InfoIcon"
-          size="18"
-          class="mr-50"
-        />
-        <span class="font-weight-bold">Information</span>
-      </template>
-
-      <account-setting-information
-        v-if="options.info"
-        :information-data="options.info"
-      />
-    </b-tab>
-
-    <!-- social links -->
-    <b-tab>
-
-      <!-- title -->
-      <template #title>
-        <feather-icon
-          icon="LinkIcon"
-          size="18"
-          class="mr-50"
-        />
-        <span class="font-weight-bold">Social</span>
-      </template>
-
-      <account-setting-social
-        v-if="options.social"
-        :social-data="options.social"
-      />
-    </b-tab>
-
-    <!-- notification -->
-    <b-tab>
-
-      <!-- title -->
-      <template #title>
-        <feather-icon
-          icon="BellIcon"
-          size="18"
-          class="mr-50"
-        />
-        <span class="font-weight-bold">Notifications</span>
-      </template>
-
-      <account-setting-notification
-        v-if="options.notification"
-        :notification-data="options.notification"
-      />
-    </b-tab>
+    
   </b-tabs>
 </template>
 
@@ -107,9 +52,6 @@
 import { BTabs, BTab } from 'bootstrap-vue'
 import AccountSettingGeneral from './AccountSettingGeneral.vue'
 import AccountSettingPassword from './AccountSettingPassword.vue'
-import AccountSettingInformation from './AccountSettingInformation.vue'
-import AccountSettingSocial from './AccountSettingSocial.vue'
-import AccountSettingNotification from './AccountSettingNotification.vue'
 
 export default {
   components: {
@@ -117,9 +59,6 @@ export default {
     BTab,
     AccountSettingGeneral,
     AccountSettingPassword,
-    AccountSettingInformation,
-    AccountSettingSocial,
-    AccountSettingNotification,
   },
   data() {
     return {
