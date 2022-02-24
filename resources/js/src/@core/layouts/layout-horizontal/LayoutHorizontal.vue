550f9f770904d9d45560b98d2579df3c916973ea
swarup <swarup@idealabz.biz>
2022-02-24 23:40:50 +0530

    theme customizer

--- resources/js/src/@core/layouts/layout-horizontal/LayoutHorizontal.vue
+++ resources/js/src/@core/layouts/layout-horizontal/LayoutHorizontal.vue
@@ -12,7 +12,7 @@
       :style="{
         backgroundColor: 'white',
         boxShadow: navbarType === 'static' && scrolledTo ? 'rgba(0, 0, 0, 0.05) 0px 4px 20px 0px' : null,
-        borderBottom: '1px solid',
+        borderBottom: navbarType === 'static' && scrolledTo && skin === 'light' ? null : '1px solid',
       }"
       :toggleable="false"
       class="header-navbar navbar-shadow align-items-center navbar-brand-center navbar-fixed"
