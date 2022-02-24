3dcdab1bf1d627685bddcca793b24c8f3950bd9a
swarup <swarup@idealabz.biz>
2022-02-25 00:26:13 +0530

    country field

--- database/migrations/2022_02_24_185356_add_country_code_on_user_table.php
+++ database/migrations/2022_02_24_185356_add_country_code_on_user_table.php
@@ -13,7 +13,10 @@ class AddCountryCodeOnUserTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('country_code')->nullable()->after('mobile');
+            $table->string('cost')->nullable()->after('country_code');
+        });
     }
 
     /**
@@ -23,6 +26,9 @@ class AddCountryCodeOnUserTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('country_code');
+            $table->dropColumn('cost');
+        });
     }
 }
