39b442fed64c498c7b3a1a3e4d9d421759d1be11
swarup <swarup@idealabz.biz>
2022-03-02 23:32:35 +0530

    notification button

--- resources/js/src/@core/layouts/components/app-navbar/components/NotificationDropdown.vue
+++ resources/js/src/@core/layouts/components/app-navbar/components/NotificationDropdown.vue
@@ -87,13 +87,13 @@
     </vue-perfect-scrollbar>
 
     <!-- Cart Footer -->
-    <!-- <li class="dropdown-menu-footer"><b-button
+    <li class="dropdown-menu-footer"><b-button
       v-ripple.400="'rgba(255, 255, 255, 0.15)'"
       variant="primary"
       block
       :to="{ name: 'notifications' }"
     >Read all notifications</b-button>
-    </li> -->
+    </li>
     <b-dropdown-item
       class="dropdown-menu-footer"
       link-class="align-items-center"
