0b5b9e1a1d0e3a353962d5ce00b18bddf4db3761
swarup <swarup@idealabz.biz>
2022-03-09 23:08:24 +0530

    fix footer allign

--- resources/js/src/@core/layouts/components/AppFooter.vue
+++ resources/js/src/@core/layouts/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="clearfix mb-0">
+  <p class="clearfix mb-0 text-center">
     <span class="float-md-left d-block d-md-inline-block mt-25">
       COPYRIGHT  © {{ new Date().getFullYear() }}
       <b-link
@@ -10,13 +10,13 @@
       <span class="d-none d-sm-inline-block">, All rights Reserved. Hand-crafted & Made with <span style="color:red;">❤️</span> by Idea Labz</span>
     </span>
 
-    <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
+    <!-- <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
       <feather-icon
         icon="HeartIcon"
         size="21"
         class="text-danger stroke-current"
       />
-    </span>
+    </span> -->
   </p>
 </template>
 
