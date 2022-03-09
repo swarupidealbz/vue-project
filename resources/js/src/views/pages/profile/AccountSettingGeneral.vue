afe3f1d194a26273eac62b5f026fad915ce75b71
swarup <swarup@idealabz.biz>
2022-03-09 19:19:29 +0530

    profile page default image

--- resources/js/src/views/pages/profile/AccountSettingGeneral.vue
+++ resources/js/src/views/pages/profile/AccountSettingGeneral.vue
@@ -210,15 +210,18 @@ export default {
       this.local = JSON.parse(JSON.stringify(this.userData))
     },
     unsetImage() {
+      console.log('unset image');
       this.image = 'unset';
       this.profileFile = null;
       this.upload();
     },
     uploadProfileImage() {
+      console.log('upload image');
       this.image ='set';
       this.upload();
     },
     upload() {
+      console.log('upload');
       var formData = new FormData();
       formData.append("profile_image", this.profileFile);
       formData.append("action", this.image);
