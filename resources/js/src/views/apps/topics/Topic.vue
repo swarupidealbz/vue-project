07c4620cd6091383832004cb57d6dd2fd9912fa4
swarup <swarup@idealabz.biz>
2022-03-20 18:58:17 +0530

    topic load more function

--- resources/js/src/views/apps/topics/Topic.vue
+++ resources/js/src/views/apps/topics/Topic.vue
@@ -299,7 +299,7 @@ export default {
   },
   methods: {
     more() {
-      this.$store.commit('setTopicMore', false);
+      this.$store.commit('app/setTopicMore', false);
       this.$store.dispatch('app/loadMoreTopic', { 
         website: this.$store.state.app.selectedWebsite.id, 
         order: this.$store.state.app.selectedOrder.id,
