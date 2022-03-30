b321e91cb1c29b8366732503b626183fe3f3d5c5
swarup <swarupanandagorai@hotmail.com>
2022-03-31 00:52:45 +0530

    update logo height

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -36,7 +36,7 @@
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
-      height="24"
+      height="50"
     >
       <defs>
         <linearGradient
